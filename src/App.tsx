d60b631cd44bc425b4f9d86734d03d9902fbfad8
George W F Santos <gweverton21@hotmail.com>
2022-10-13 13:17:09 -0300

    Change h1 text in the App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import "./styles/global.css";
 
 export function App() {
-  return <h1 className="font-bold text-2xl text-violet-500">App</h1>;
+  return (
+    <h1 className="font-bold text-2xl text-violet-500">My Design System</h1>
+  );
 }
