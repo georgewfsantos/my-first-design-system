e99ae06300a4bd622e74f08e44b6eb294c72991e
George W F Santos <gweverton21@hotmail.com>
2022-10-13 17:15:32 -0300

    Build web interface using the design system componets

--- src/components/Checkbox.tsx
+++ src/components/Checkbox.tsx
@@ -4,11 +4,14 @@ import { Check } from "phosphor-react";
 
 import * as RadixCheckbox from "@radix-ui/react-checkbox";
 
-export type CheckboxProps = {};
+export type CheckboxProps = RadixCheckbox.CheckboxProps & {};
 
-export function Checkbox({}: CheckboxProps) {
+export function Checkbox(props: CheckboxProps) {
   return (
-    <RadixCheckbox.Root className="w-6 h-6 p-[2px] bg-gray-800 rounded">
+    <RadixCheckbox.Root
+      className="w-6 h-6 p-[2px] bg-gray-800 rounded"
+      {...props}
+    >
       <RadixCheckbox.Indicator asChild>
         <Check weight="bold" className="w-5 h-5 text-cyan-500" />
       </RadixCheckbox.Indicator>
